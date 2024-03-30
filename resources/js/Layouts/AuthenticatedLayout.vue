aa54fd4367ea0e499f3773a9f68f09c46eda5f9f
ibrahimIJ-1 <ibrahimjindybuisness@gmail.com>
2024-03-30 15:37:24 +0300

    locale updated

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
@@ -8,10 +8,13 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
 import Notification from "@/Components/Notification.vue";
 import ErrorDialog from "@/Components/ErrorDialog.vue";
-import { loadLanguageAsync } from 'laravel-vue-i18n';
+import { getActiveLanguage, loadLanguageAsync } from 'laravel-vue-i18n';
 
 const showingNavigationDropdown = ref(false);
-
+const locale = ref('en')
+onMounted(() => {
+    locale.value = getActiveLanguage()
+})
 const changeLocale = async (event) => {
     await loadLanguageAsync(event.target.value)
 }
@@ -68,29 +71,6 @@ const changeLocale = async (event) => {
                                             <DropdownLink :href="route('logout')" method="post" as="button">
                                                 Log Out
                                             </DropdownLink>
-                                        </template>
-                                    </Dropdown>
-                                </div>
-                            </div>
-
-                            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                                <!-- Settings Dropdown -->
-                                <div class="ms-3 relative">
-                                    <Dropdown align="right" width="48">
-                                        <template #trigger>
-                                            <span class="inline-flex rounded-md">
-                                                <button type="button"
-                                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                                    {{ $page.props.auth.user.name }}
-
-                                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fill-rule="evenodd"
-                                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                            clip-rule="evenodd" />
-                                                    </svg>
-                                                </button>
-                                            </span>
                                         </template>
                                     </Dropdown>
                                 </div>
